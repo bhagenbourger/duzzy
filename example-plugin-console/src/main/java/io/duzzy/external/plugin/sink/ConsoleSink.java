b3648b02734aaac7a6b702d14e4b5573864f7858
benoit <benoit@hagenbourger.fr>
2024-11-18 21:11:46 +0100

    POC external module

---
+++ example-plugin-console/src/main/java/io/duzzy/external/plugin/sink/ConsoleSink.java
@@ -0,0 +1,24 @@
+package io.duzzy.external.plugin.sink;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.duzzy.core.Serializer;
+import io.duzzy.core.sink.OutputStreamSink;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ConsoleSink extends OutputStreamSink {
+
+    @JsonCreator
+    public ConsoleSink(@JsonProperty("serializer") Serializer<?> serializer) {
+        super(serializer, new ByteArrayOutputStream());
+    }
+
+    @Override
+    public void close() throws IOException {
+        serializer.close();
+        System.out.writeBytes(((ByteArrayOutputStream) outputStream).toByteArray());
+        //Don't close System.out
+    }
+}
