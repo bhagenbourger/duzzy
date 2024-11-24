685383ec6e9821c0148a8210b7676e729ff826ac
benoit <benoit@hagenbourger.fr>
2024-11-24 15:26:03 +0100

    Add documentation

--- cli/src/main/java/io/duzzy/cli/output/DuzzyResultXmlFormatter.java
+++ cli/src/main/java/io/duzzy/cli/output/DuzzyResultXmlFormatter.java
@@ -1,13 +1,13 @@
-package io.duzzy.cli;
+package io.duzzy.cli.output;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.duzzy.core.DuzzyResult;
 
-public class DuzzyResultYamlFormatter implements DuzzyResultFormatter {
-  private static final ObjectMapper MAPPER = new YAMLMapper()
+public class DuzzyResultXmlFormatter implements DuzzyResultFormatter {
+  private static final ObjectMapper MAPPER = new XmlMapper()
       .registerModule(new JavaTimeModule());
 
   @Override
