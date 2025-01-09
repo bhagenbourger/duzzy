75f1c38f870700812e42c2dcf0183463c3a770c5
bhagenbourger <benoit@hagenbourger.fr>
2025-01-09 11:03:17 +0100

    Create local file if not exists (#63)

--- core/src/main/java/io/duzzy/plugin/sink/LocalFileSink.java
+++ core/src/main/java/io/duzzy/plugin/sink/LocalFileSink.java
@@ -1,19 +1,40 @@
 package io.duzzy.plugin.sink;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.duzzy.core.serializer.Serializer;
 import io.duzzy.core.sink.Sink;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class LocalFileSink extends Sink {
 
   @JsonCreator
   public LocalFileSink(
-      @JsonProperty("serializer") Serializer<?> serializer,
-      @JsonProperty("filename") String filename
-  ) throws FileNotFoundException {
-    super(serializer, new FileOutputStream(filename));
+      @JsonProperty("serializer")
+      Serializer<?> serializer,
+      @JsonProperty("filename")
+      String filename,
+      @JsonProperty("create_if_not_exists")
+      @JsonAlias({"createIfNotExists", "create-if-not-exists"})
+      Boolean createIfNotExists
+  ) throws IOException {
+    super(serializer, buildFileOutputStream(filename, createIfNotExists));
+  }
+
+  private static FileOutputStream buildFileOutputStream(
+      String filename,
+      Boolean createIfNotExists
+  ) throws IOException {
+    if (createIfNotExists != null && createIfNotExists) {
+      final Path folder = Path.of(filename).getParent();
+      if (folder != null && !Files.exists(folder)) {
+        Files.createDirectories(folder);
+      }
+    }
+    return new FileOutputStream(filename);
   }
 }
--- core/src/test/java/io/duzzy/plugin/sink/LocalFileSinkTest.java
+++ core/src/test/java/io/duzzy/plugin/sink/LocalFileSinkTest.java
@@ -5,16 +5,14 @@ import static io.duzzy.tests.Data.getDataOne;
 import static io.duzzy.tests.Data.getDataTwo;
 import static io.duzzy.tests.Helper.getFromResources;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
 import io.duzzy.core.DuzzyContext;
 import io.duzzy.core.sink.Sink;
 import io.duzzy.plugin.serializer.JsonSerializer;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
@@ -30,12 +28,27 @@ public class LocalFileSinkTest {
   }
 
   @Test
+  void parsedFromYamlFailed() {
+    final File sinkFile = getFromResources(getClass(), "sink/local-file-sink-error.yaml");
+
+    assertThatThrownBy(() -> YAML_MAPPER.readValue(sinkFile, Sink.class))
+        .isInstanceOf(ValueInstantiationException.class)
+        .hasMessage("Cannot construct instance of `io.duzzy.plugin.sink.LocalFileSink`, "
+            + "problem: build/failed/test.json (No such file or directory)"
+            + "\n at [Source: UNKNOWN; byte offset: #UNKNOWN]");
+  }
+
+  @Test
   void writeJson() throws Exception {
     final String filename = "build/test.json";
     final String expected =
         "{\"c1\":1,\"c2\":\"one\"}\n{\"c1\":2,\"c2\":\"two\"}\n{\"c1\":2,\"c2\":\"two\"}";
 
-    final LocalFileSink localFileSink = new LocalFileSink(new JsonSerializer(), filename);
+    final LocalFileSink localFileSink = new LocalFileSink(
+        new JsonSerializer(),
+        filename,
+        true
+    );
     localFileSink.init(DuzzyContext.DEFAULT.schemaContext());
     localFileSink.write(getDataOne());
     localFileSink.write(getDataTwo());
