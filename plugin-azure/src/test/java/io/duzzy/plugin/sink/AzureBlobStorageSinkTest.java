4bbdb101090a7efa12d9424f9953c39b4bcd595f
bhagenbourger <benoit@hagenbourger.fr>
2025-06-30 22:11:49 +0200

    Add Azure Blob and Datalake Storage sinks

---
+++ plugin-azure/src/test/java/io/duzzy/plugin/sink/AzureBlobStorageSinkTest.java
@@ -0,0 +1,110 @@
+package io.duzzy.plugin.sink;
+
+import static io.duzzy.core.sink.FileSink.addFilePart;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.storage.blob.BlobServiceVersion;
+import io.duzzy.core.serializer.Serializer;
+import io.duzzy.core.sink.Sink;
+import io.duzzy.plugin.serializer.JsonSerializer;
+import io.duzzy.tests.Data;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.azure.AzuriteContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class AzureBlobStorageSinkTest {
+
+  private static final AzuriteContainer azurite =
+      new AzuriteContainer(DockerImageName.parse("mcr.microsoft.com/azure-storage/azurite:3.33.0"));
+
+  @BeforeAll
+  static void beforeAll() {
+    azurite.start();
+  }
+
+  @AfterAll
+  static void afterAll() {
+    azurite.stop();
+  }
+
+  @Test
+  public void testSink() throws Exception {
+    final String containerName = "blob-container";
+    final String path = "blob.json";
+    final String path1 = "blob_1.json";
+    final AzureBlobStorageSink sink = new MockAzureBlobStorageSink(
+        new JsonSerializer(),
+        "devstoreaccount1",
+        true,
+        containerName,
+        path
+    );
+
+    sink.init(null);
+    sink.write(Data.getDataOne());
+    sink.write(Data.getDataTwo());
+    sink.close();
+
+    final Sink fork = sink.fork(1L);
+    fork.init(null);
+    fork.write(Data.getDataOne());
+    fork.write(Data.getDataTwo());
+    fork.close();
+
+    final BlobContainerClient blobContainerClient = new BlobContainerClientBuilder()
+        .connectionString(azurite.getConnectionString())
+        .serviceVersion(BlobServiceVersion.V2025_01_05)
+        .containerName(containerName)
+        .buildClient();
+
+    final String result = blobContainerClient.getBlobClient(path).downloadContent().toString();
+    final String result1 = blobContainerClient.getBlobClient(path1).downloadContent().toString();
+
+
+    assertEquals("{\"c1\":1,\"c2\":\"one\"}\n{\"c1\":2,\"c2\":\"two\"}", result);
+    assertEquals("{\"c1\":1,\"c2\":\"one\"}\n{\"c1\":2,\"c2\":\"two\"}", result1);
+  }
+
+  private static class MockAzureBlobStorageSink extends AzureBlobStorageSink {
+
+    public MockAzureBlobStorageSink(
+        Serializer<?> serializer,
+        String accountName,
+        Boolean createContainerIfNotExists,
+        String container,
+        String blobName
+    ) {
+      super(
+          serializer,
+          accountName,
+          createContainerIfNotExists,
+          container,
+          blobName
+      );
+    }
+
+    @Override
+    public Sink fork(Long threadId) throws Exception {
+      return new MockAzureBlobStorageSink(
+          getSerializer().fork(threadId),
+          getAccountName(),
+          getCreateContainerIfNotExists(),
+          getContainer(),
+          addFilePart(getBlobName(), threadId)
+      );
+    }
+
+    @Override
+    protected BlobContainerClient buildBlobContainerClient() {
+      return new BlobContainerClientBuilder()
+          .connectionString(azurite.getConnectionString())
+          .serviceVersion(BlobServiceVersion.V2025_01_05)
+          .containerName(getContainer())
+          .buildClient();
+    }
+  }
+}
